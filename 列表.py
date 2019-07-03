ba2e584a908c9e0fb7554dee87726a9449d312a6
Rongpu Huang <hrp8888@outlook.com>
2019-07-03 17:18:09 +0800

    HPF P13

---
+++ 列表.py
@@ -0,0 +1,11 @@
+games = ["StarCraft2","Dota 2","OverWatch"]
+print(games)
+print(len(games))
+print(games[0])#列表是从0开始的
+#Head First Python Page13
+games.insert(1, "2010")
+print(games)
+games.insert(3,"2013")
+print(games)
+games.append(2016)
+print(games)
