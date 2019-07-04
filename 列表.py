c770decc2b676b6f61bdfc1a9346ee1f09c65d00
Rongpu Huang <hrp8888@outlook.com>
2019-07-04 23:09:18 +0800

    adding if else

--- 列表.py
+++ 列表.py
@@ -1,11 +1,22 @@
-games = ["StarCraft2","Dota 2","OverWatch"]
+games=["StarCraft2",[
+    "Blizzard Entertainment" ,"RTS"
+],"Dota 2","OverWatch"]
 print(games)
 print(len(games))
 print(games[0])#列表是从0开始的
 #Head First Python Page13
 games.insert(1, "2010")
-print(games)
+
 games.insert(3,"2013")
 print(games)
 games.append(2016)
 print(games)
+
+#HPF P14:在小列表中可以从头创建列表，避免计算
+#HPF P15:
+for each_flick in games:#有关each_flick（目标标识符）更多 HPF P16参考
+    print(each_flick)
+    
+#HPF P18:
+print(games[2][0])#嵌套列表显示
+
