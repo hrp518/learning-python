ce4eb67ee95ccaeb23664bc0c841cc9f3c69e790
Rongpu Huang <hrp8888@outlook.com>
2019-07-07 20:26:41 +0800

    Reupdate in ShaoGuan

--- 创建一个函数.py
+++ 创建一个函数.py
@@ -1,7 +1,7 @@
 def print_list(the_list):
     for item in the_list:
         if isinstance(item,list):
-            print_list(each item)
+            print_list(item)
         else:
             print(item)#HFP P30
 
